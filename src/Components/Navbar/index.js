5a65674b7ea5da87b7e0d02fbf79a3c7e4967124
Arman Rostami <arman.rostami.999@gmail.com>
2019-01-02 01:42:27 +0330

    Design navbar

---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './style.css';
+import logo from '../../images/logo.png';
+import { withRouter } from 'react-router-dom';
+
+class Navbar extends React.Component {
+
+constructor(props) {
+    super(props);
+    this.handleLogout = this.handleLogout.bind(this);
+}
+
+handleLogout (event) {
+    event.preventDefault();
+    localStorage.clear();
+    this.props.history.push('/');
+}
+
+render() {
+    return ( 
+    <div className="navbar">
+        <ul className = "navbar">
+            <li className = "logout">
+                <a href="/logout" onClick={e=>this.handleLogout(e)} >logout</a>
+            </li>
+            <li>
+                <a href="/" >
+                <img className="nav_logo" src={logo} alt="logo"/>
+                </a>
+            </li>
+        </ul>
+    </div> 
+        )
+    }
+}
+
+export default withRouter(Navbar);
