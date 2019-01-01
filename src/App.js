5a9832075f373cfeddeec6ebab0c3e8278101275
Arman Rostami <arman.rostami.999@gmail.com>
2019-01-02 00:38:38 +0330

    Add authenticatin check to routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Home from './Components/Home';
 import Signup from './Components/Signup';
 import Login from './Components/Login';
+import NotFound from './Components/NotFound';
+import Dashboard from './Components/Dashboard';
 import Auth from './Auth';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faIgloo } from '@fortawesome/free-solid-svg-icons';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { connect } from 'react-redux';
+
+function mapStateToProps (state) {
+  return {
+    authenticated: state.authenticated
+  };
+}
 
 class App extends Component {
   render() {
@@ -16,9 +25,31 @@ class App extends Component {
         <BrowserRouter>
           <Auth>
             <Switch>
-              <Route exact path = {'/'} component = {Home} />
-              <Route exact path = {'/signup'} component = {Signup} />
-              <Route exact path = {'/login'} component = {Login} />
+              <Route exact path = {'/'}
+                render={() =>
+                  this.props.authenticated ? (
+                    <Dashboard />
+                  ) : (
+                    <Home />
+                  )
+                } />
+              <Route exact path = {'/signup'}
+                render={() =>
+                  this.props.authenticated ? (
+                    <Redirect to="/" />
+                  ) : (
+                    <Signup />
+                  )
+                } />
+              <Route exact path = {'/login'}
+                render={() =>
+                  this.props.authenticated ? (
+                    <Redirect to="/" />
+                  ) : (
+                    <Login />
+                  )
+                } />
+              <Route component = {NotFound} />
             </Switch>
           </Auth>
         </BrowserRouter>
@@ -30,4 +61,4 @@ class App extends Component {
 
 library.add(faIgloo)
 
-export default App;
+export default connect(mapStateToProps)(App);
